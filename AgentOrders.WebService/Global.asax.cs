9d2413154f50ad3d239d05c5155314200d9b227b
Pavel <pavel@liram.com>
2022-05-02 14:32:15 +0300

    Basic app build

---
+++ AgentOrders.WebService/Global.asax.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.Http;
+
+namespace AgentOrders.WebService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
