3afde35718d93e371ec153c1d36f9e589035040b
Pavel <pavel@liram.com>
2022-05-02 16:08:53 +0300

    1) Created basic project configuration; 2) Created api method stubs

---
+++ AgentOrders.WebService/Models/ListByMinOrdersResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AgentOrders.WebService.Models
+{
+    public class ListByMinOrdersResponseModel
+    {
+        public string AgentCode { get; set; }
+        public string AgentName { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
