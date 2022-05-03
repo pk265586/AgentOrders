fafba3edb6d40860c2674e8a9f19eb0408542b2a
Pavel <pavel@liram.com>
2022-05-03 18:16:41 +0300

    Moved order-related APIs to new OrderController; clarified api method descriptions

--- AgentOrders.WebService/Controllers/AgentController.cs
+++ AgentOrders.WebService/Controllers/AgentController.cs
@@ -10,12 +10,10 @@ namespace AgentOrders.WebService.Controllers
     public class AgentController : ApiController
     {
         IAgentService agentService;
-        IOrderService orderService;
 
-        public AgentController(IAgentService agentService, IOrderService orderService) 
+        public AgentController(IAgentService agentService) 
         {
             this.agentService = agentService;
-            this.orderService = orderService;
         }
 
         [HttpGet]
@@ -38,35 +36,5 @@ namespace AgentOrders.WebService.Controllers
             return new { AgentCode = agentCode };
         }
 
-        /// <summary>
-        /// Returns list of agents, joined with N-th oldest order for each agent. If an agent has less than N orders, returns empty order values for that agent (left join).
-        /// <para> Sample url for testing: https://localhost:44358/api/Agent/OrdersByIndex?agentCodes=A004,A009&orderIndex=2 </para>
-        /// </summary>
-        /// <param name="agentCodes"> Agent codes filter </param>
-        /// <param name="orderIndex"> Order index to return for each agent </param>
-        /// <returns> The list of the agents with orders, no more than 1 order per agent </returns>
-        [HttpGet]
-        public OrdersByIndexResponseModel[] OrdersByIndex(string agentCodes, int orderIndex)
-        {
-            var codesArray = agentCodes.Split(',');
-            return orderService.GetOrdersByIndex(codesArray, orderIndex).
-                Select(m => ModelConvertor.AgentOrderToResponseModel(m))
-                .ToArray();
-        }
-
-        /// <summary>
-        /// Returns list of agents who have orders count in the given year more or equal to the given number.
-        /// <para> Sample url for testing: https://localhost:44358/api/Agent/ListByMinOrders?minCount=5&year=2021 </para>
-        /// </summary>
-        /// <param name="minCount"> Minimum number of orders of an agent </param>
-        /// <param name="year"> A year to check </param>
-        /// <returns> The list of the agents in question </returns>
-        [HttpGet]
-        public ListByMinOrdersResponseModel[] ListByMinOrders(int minCount, int year) 
-        {
-            return orderService.GetAgentsByMinOrders(minCount, year).
-                Select(m => ModelConvertor.AgentToResponseModel(m))
-                .ToArray();
-        }
     }
 }
