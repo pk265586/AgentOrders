3be7daf44d5cd572de86787314639bcddf8f1691
Pavel <pavel@liram.com>
2022-05-04 14:59:12 +0300

    Implemented api key handler

--- AgentOrders.Logic/AppSettings.cs
+++ AgentOrders.Logic/AppSettings.cs
@@ -6,5 +6,6 @@ namespace AgentOrders.Logic
     public static class AppSettings
     {
         public static string ConnectionString => ConfigurationManager.ConnectionStrings["Main"].ConnectionString;
+        public static string ApiKey => ConfigurationManager.AppSettings["ApiKey"];
     }
 }
--- AgentOrders.WebService/Global.asax.cs
+++ AgentOrders.WebService/Global.asax.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Web.Http;
 
+using AgentOrders.WebService.Infrastructure;
+
 namespace AgentOrders.WebService
 {
     public class WebApiApplication : System.Web.HttpApplication
@@ -8,6 +10,7 @@ namespace AgentOrders.WebService
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new AuthorizationHandler());
             AppConfig.Initialize();
         }
     }
